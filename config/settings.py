6a9eb1b68d929efc8eff6d3fb642435002b94f3b
손정빈 <jbjbjb@sonjeongbin-ui-MacBookPro.local>
2026-03-26 16:56:33 +0900

    fix : API base url, Target list url

--- config/settings.py
+++ config/settings.py
@@ -10,4 +10,5 @@ TOR_PROXY_LIST = [f"socks5h://127.0.0.1:{port}" for port in range(9050, 9060)]
 
 TARGET_LIST_URL = os.getenv("TARGET_LIST_URL")
 API_BASE_URL = os.getenv("API_BASE_URL")
-API_JWT_TOKEN = os.getenv("API_JWT_TOKEN")
+API_INGESTION_KEY = os.getenv("API_INGESTION_KEY")
+
